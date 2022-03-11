ff365e1b1107065b2e5cfef121fcf943fbc745d5
tarik <63872091+tariksitki@users.noreply.github.com>
2022-03-12 00:57:44 +0100

    Project completed

---
+++ App.js
@@ -0,0 +1,34 @@
+
+
+    let hour = document.getElementById("hour");
+    let minute = document.getElementById("minute");
+    let second = document.getElementById("second");
+    let colon = document.querySelector(".colon");
+
+setInterval(function() {
+    second.innerText = (Number(second.innerText) + 1).toString().padStart(2, 0);
+    let clock = `${hour.innerText}:${minute.innerText}:${second.innerText}`;
+
+    if (second.innerText == 60) {
+        minute.innerText = (Number(minute.innerText) + 1).toString().padStart(2, 0);
+        second.innerText = "00";
+    }
+
+    if (minute.innerText == 60) {
+        hour.innerText = (Number(hour.innerText) + 1).toString().padStart(2, 0);
+        minute.innerText = "00";
+    }
+
+    if (hour.innerText == 23 && minute.innerText == 59 && second.innerText == 60) {
+        hour.innerText = "00";
+        minute.innerText = "00";
+        second.innerText = "00";
+    };
+
+    colon.classList.toggle("hidden");
+    
+}, 1000);
+
+
+
+
